5ef3468ae82dc71107be3dbf0476cdc762884e1c
jhonatan costa <jhonatan.s.costa97@gmail.com>
2025-02-18 21:53:06 -0300

    feat(app): Start working in my portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'about',
+    title: 'About',
+    loadChildren: () => import('./views/about/about.routes').then(m => m.aboutRoutes),
+  },
+  {
+    path: 'contact',
+    title: 'Contact',
+    loadChildren: () => import('./views/contact/contact.routes').then(m => m.contactRoutes),
+  },
+  {
+    path: 'home',
+    title: 'Home',
+    loadChildren: () => import('./views/home/home.routes').then(m => m.homeRoutes),
+  },
+  {
+    path: 'projects',
+    title: 'Projects',
+    loadChildren: () => import('./views/projects/projects.routes').then(m => m.projectsRoutes),
+  },
+  {
+    path: 'testimonials',
+    title: 'Testimonials',
+    loadChildren: () => import('./views/testimonials/testimonials.routes').then(m => m.testimonialsRoutes),
+  },
+  {
+    path: '**',
+    title: '404',
+    loadChildren: () => import('./views/not-found/not-found.routes').then(m => m.notFoundRoutes),
+  }
+];
