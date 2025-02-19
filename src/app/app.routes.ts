0a1d349bd00e1d79a9b507f7c808ecea8c8ef778
jhonatan costa <jhonatan.s.costa97@gmail.com>
2025-02-18 22:32:59 -0300

    fix(custom-theme): Fixed the custom-theme import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,32 +8,32 @@ export const routes: Routes = [
   },
   {
     path: 'about',
-    title: 'About',
+    title: 'Jhonatan - About',
     loadChildren: () => import('./views/about/about.routes').then(m => m.aboutRoutes),
   },
   {
     path: 'contact',
-    title: 'Contact',
+    title: 'Jhonatan - Contact',
     loadChildren: () => import('./views/contact/contact.routes').then(m => m.contactRoutes),
   },
   {
     path: 'home',
-    title: 'Home',
+    title: 'Jhonatan - Home',
     loadChildren: () => import('./views/home/home.routes').then(m => m.homeRoutes),
   },
   {
     path: 'projects',
-    title: 'Projects',
+    title: 'Jhonatan - Projects',
     loadChildren: () => import('./views/projects/projects.routes').then(m => m.projectsRoutes),
   },
   {
     path: 'testimonials',
-    title: 'Testimonials',
+    title: 'Jhonatan - Testimonials',
     loadChildren: () => import('./views/testimonials/testimonials.routes').then(m => m.testimonialsRoutes),
   },
   {
     path: '**',
-    title: '404',
+    title: 'Jhonatan - 404',
     loadChildren: () => import('./views/not-found/not-found.routes').then(m => m.notFoundRoutes),
   }
 ];
