32511c817f7d0456a9c02ea0f4672c3c30a9e9ae
jhonatan <jhonatan.s.costa97@gmail.com>
2025-10-03 16:37:23 -0300

    feat(testimonials): Fixed the profile images bugs and added the new testimonial

--- src/app/views/testimonials/testimonial.data.ts
+++ src/app/views/testimonials/testimonial.data.ts
@@ -2,37 +2,67 @@ import {TestimonialInterface} from './testimonial.interface';
 
 export const testimonialData: TestimonialInterface[] = [
   {
-    picture: 'https://media.licdn.com/dms/image/v2/C4D03AQHh7j4MPLgUNQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1600456238398?e=1746057600&v=beta&t=Mk6CThUojlbHcEmAWI6-bCWqOOWEvKsGAOXW_m2aZb4',
+    picture: 'assets/img/testimonials/david.png',
+    name: 'David Pereira',
+    linkedin: 'https://www.linkedin.com/in/davidrtpereira',
+    position: 'CTO at @Loginfo and CTO at @Priime Tech',
+    testimonial: `In a team, I had the honor of leading Jhonatan,
+      one of the most competent and exceptional development professionals I have ever worked with.
+      His excellence translates into high-speed, high-quality work, always aligned with a deep sense
+      of responsibility toward the project.' + 'What sets him apart in a remarkable way is his
+      communication skills and resilience, attributes rarely found with such solidity. In addition
+      to possessing unquestionable technical mastery, Jhonatan is a proactive collaborator who actively
+      contributes with constructive criticism and well-founded opinions, enriching both the debate
+      and the project. His contribution and the effectiveness of his work exceed expectations, and
+      his humility and remarkable team spirit make him an invaluable professional. It is with complete
+      conviction that I recommend Jhonatan, and I would not hesitate to have him on my team again.`
+  },
+  {
+    picture: 'assets/img/testimonials/jaq.jpeg',
     name: 'Jacqueline Feitosa Garcia',
     linkedin: 'https://www.linkedin.com/in/jacquelinefeitosagarcia',
     position: 'Talent | Project Manager',
     testimonial: `I've been working with Jhonatan for a few months now, and I'm glad to have him on the team.
-  He is always one step ahead when it comes to participating and translating the client's needs
-  to fitable and doable development solutions. He is always ready to help and is extremely proactive,
-  organized, communicative and seeking for quality, which is so important to the business, to the team's
-  atmosphere and to get things done more effectively and in a timely manner! And I'm pretty sure that if Jhonatan
-  keeps this hands-on posture up and openness to learn, he'll color even more this beautiful journey he's been writing.`
+      He is always one step ahead when it comes to participating and translating the client's needs
+      to fitable and doable development solutions. He is always ready to help and is extremely proactive,
+      organized, communicative and seeking for quality, which is so important to the business, to the team's
+      atmosphere and to get things done more effectively and in a timely manner! And I'm pretty sure that if Jhonatan
+      keeps this hands-on posture up and openness to learn, he'll color even more this beautiful journey he's been writing.`
   },
   {
-    picture: 'https://media.licdn.com/dms/image/v2/C4D03AQGj-6Vk55CEoQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1616515013255?e=1746057600&v=beta&t=d81mwYX5ddItKrFkh_ZNXB7_Txj28NUYSX-9CdzfQiA',
-    name: 'Daniel Francisco',
-    linkedin: 'https://www.linkedin.com/in/daniel-francisco-79ab99165',
-    position: 'Product Manager',
-    testimonial: 'Jhonatan is an exceptional professional who finds solutions to any problem, no matter how complex it is, that comes his way during his career in software development. A major differentiator, beyond his extensive knowledge of technologies, experience, and determination, is his clarity in communication and proactivity, which are key components for the success of any project. Since I started working with Jhonatan, I have always felt his sense of teamwork, his dedication, and his relentless pursuit of excellence.'
-  },
-  {
-    picture: 'https://media.licdn.com/dms/image/v2/D4D03AQFDlUj6AuVbQw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1724443204442?e=1746057600&v=beta&t=Q55C9ijpUZJM_LJA4NFYk-tNg-6FCCvq6IltXIK9j18',
+    picture: 'assets/img/testimonials/arno.jpeg',
     name: 'Arno Klettenberg Neto',
     linkedin: 'https://www.linkedin.com/in/arno-klettenberg-neto-b987a818a',
     position: 'UX/UI Designer | Product Owner',
-    testimonial: 'Jhonatan has always been a very dedicated, helpful, and communicative professional. In the project we worked on together, he was an essential part of delivering quality in a complex and fast-changing product.'
+    testimonial: `Jhonatan has always been a very dedicated, helpful, and communicative professional.
+      In the project we worked on together, he was an essential part of delivering quality in a
+      complex and fast-changing product.`
   },
   {
-    picture: 'https://media.licdn.com/dms/image/v2/D4D03AQGy7DE4ZVBe6A/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1718256108240?e=1746057600&v=beta&t=Ju1r2Jlcco3hT9OMRoIFufJWfW2Qw0HMw2d5wuSvobQ',
+    picture: 'assets/img/testimonials/cris.jpeg',
     name: 'Cristiane Paiva Lima',
     linkedin: 'https://www.linkedin.com/in/cristianeepaivaa',
     position: 'SaaS Operations Leader @epays | Co-Founder @ohubdev',
-    testimonial: 'In our journey with Jho at TJLIT, we noticed that he is a professional focused on Front End. During his time with us, he demonstrated mastery in building Websites and Online Stores with the WordPress and Woocommerce platform, and he enjoys choosing screen layouts with the available options in the Elementor plugin and extensions. He is communicative, always calm, and willing to execute tasks. He likes to share the information he learns. I have always told him that he has the gift for teaching, he just needs to invest in it. Keep it up, Jho, always studying and dedicating yourself to what you do. Success!'
-  }
+    testimonial: `In our journey with Jho at TJLIT, we noticed that he is a professional
+      focused on Front End. During his time with us, he demonstrated mastery in building
+      Websites and Online Stores with the WordPress and Woocommerce platform, and he
+      enjoys choosing screen layouts with the available options in the Elementor plugin
+      and extensions. He is communicative, always calm, and willing to execute tasks.
+      He likes to share the information he learns. I have always told him that he has
+      the gift for teaching, he just needs to invest in it. Keep it up, Jho, always
+      studying and dedicating yourself to what you do. Success!`
+  },
+  {
+    picture: 'assets/img/testimonials/default-profile-icon.png',
+    name: 'Daniel Francisco',
+    linkedin: 'https://www.linkedin.com/in/daniel-francisco-79ab99165',
+    position: 'Product Manager',
+    testimonial: `Jhonatan is an exceptional professional who finds solutions to any problem,
+      no matter how complex it is, that comes his way during his career in software development.
+      A major differentiator, beyond his extensive knowledge of technologies, experience, and
+      determination, is his clarity in communication and proactivity, which are key components
+      for the success of any project. Since I started working with Jhonatan, I have always felt
+      his sense of teamwork, his dedication, and his relentless pursuit of excellence.`
+  },
 
 ];
